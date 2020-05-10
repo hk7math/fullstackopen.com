0695be96ae775d5a2d6ae202205fa4e99b42215a
hk7math <58369628+hk7math@users.noreply.github.com>
2020-05-11 01:54:59 +0800

    courseinfo

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => (
+  <h1>{props.course}</h1>
+) 
+
+const Content = (props) => (
+  <div>
+    <Part part={props.parts[0]} exercise={props.exercise[0]}/>
+    <Part part={props.parts[1]} exercise={props.exercise[1]}/>
+    <Part part={props.parts[2]} exercise={props.exercise[2]}/>
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part} {props.exercise}
+  </p>
+)
+
+const Total = (props) => (
+  <p>Number of exercises {props.total}</p>
+)
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content parts={[part1, part2, part3]} exercise={[exercises1, exercises2, exercises3]}/>
+      <Total total={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
