2cca02520e16ce7202e8d29250f4f8be18a0fc6b
ShepherdNg <hk7math@gmail.com>
2020-06-07 00:13:02 +0800

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [ persons, setPersons ] = useState([{ 
+    name: 'Arto Hellas',
+    number: '040-1234567'
+  }]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addName = (e) => {
     e.preventDefault()
     const name = newName.trim()
+    const number = newNumber.trim()
     if ( persons.filter( person => person.name === name).length ){
       alert(`${name} is already added to phonebook`)
     } else {
-      const newPerson = { name }
+      const newPerson = { name, number }
       setPersons(persons.concat(newPerson))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -26,11 +30,14 @@ const App = () => {
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)}/>
+        </div>
+        <div>
           <button type="submit" onClick={addName}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name}</div>)}
+      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
     </div>
   )
 }
