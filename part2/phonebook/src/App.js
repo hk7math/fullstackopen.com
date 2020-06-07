fae95598294db1a4e6ce80c767debe4faffe04e2
ShepherdNg <hk7math@gmail.com>
2020-06-07 09:18:27 +0800

    2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ search, setSearch ] = useState('')
+
+  useEffect( () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then( res => {
+        setPersons(res.data)
+      })
+  },[])
 
   const addName = (e) => {
     e.preventDefault()
@@ -36,6 +40,7 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <Filter search = {search} setSearch = {setSearch}/>
+
       <h3>add a new</h3>
       <PersonForm 
         newName = {newName}
@@ -44,6 +49,7 @@ const App = () => {
         setNewNumber = {setNewNumber}
         addName = {addName}
       />
+      
       <h3>Numbers</h3>
       <Persons personsToShow = {personsToShow} />
     </div>
